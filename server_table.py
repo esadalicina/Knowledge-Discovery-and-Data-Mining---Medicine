0c505f177fab920a17e9c4d87821a3f3e6400965
BalGu <52451260+Bal-Gu@users.noreply.github.com>
2023-04-11 00:54:23 +0200

    Made server_tables work

--- server_table.py
+++ server_table.py
@@ -1,65 +1,117 @@
+import numpy as np
 from flask import Flask, render_template, request
 import pandas as pd
-from assosiation import df_results
 
 app = Flask(__name__)
+start = 0
+length = 0
+order = 0
+leftAttribute = "None"
+rigthAttribute = "None"
+search_lift = ">0"
+search_confidence = ">0"
+direction = True
 
 
 @app.route('/')
 def index():
-    return render_template('server_table.html', title='Assosiation Table')
+    with open("association2NoFilter.csv", "r") as f:
+        df = pd.read_csv(f)
+    column_names = list(df.columns)
+
+    # Get the unique values in each column
+    unique_values = {}
+    for column in column_names:
+        unique_values[column] = list(df[column].unique())
+    first = df["lhs_attribute"].unique()
+    second = df["rhs_attribute"].unique()
+    first = np.insert(np.asarray(first), 0, "None")
+    second = np.insert(np.asarray(second), 0, "None")
+
+    return render_template('server_table.html', title='Assosiation Table', column_names=first,
+                           r_column_names=second)
 
 
 @app.route('/api/data', methods=['GET', 'POST'])
 def api_data():
-    data_filter = df_results
+    global start, length, direction, order, leftAttribute, rigthAttribute, search_confidence, search_lift
+    with open("association2NoFilter.csv", "r") as f:
 
+        data_filter = pd.read_csv(f)
+
+    pre_len = len(data_filter)
+    # TODO query the 5 textfields and filter accordingly
     # Get the search query
-    search_lift = request.args.get('lift_search')
-    search_confidence = request.args.get('confidence_search')
-    print(search_lift)
-    print(search_confidence)
 
-    # Filter the data if a search query is provided
-    if search_lift or search_confidence:
-        lift_filter = data_filter['lift'].apply(str).str.lower().str.contains(
-            search_lift.lower()) if search_lift else pd.Series(True, index=data_filter.index)
-        confidence_filter = data_filter['confidence'].apply(str).str.lower().str.contains(
-            search_confidence.lower()) if search_confidence else pd.Series(True, index=data_filter.index)
-        data_filter = data_filter[lift_filter & confidence_filter]
+    search_lift_ = request.args.get('lift_search')
+    search_confidence_ = request.args.get('confidence_search')
+    if search_lift_ is not None or search_lift != "":
+        if search_lift_ != "":
+            try:
+                data_filter = data_filter.query('lift {}'.format(search_lift_))
+                print(data_filter)
+                search_lift = search_lift_
+            except:
+                try:
+                    data_filter = data_filter.query('lift {}'.format(search_lift))
+                except:
+                    pass
+        else:
+            search_lift = ""
 
+    if search_confidence_ is not None:
+        if search_confidence_ != "":
+            try:
+                data_filter = data_filter.query('confidence'.format(search_confidence_))
+                search_lift = search_confidence_
+            except:
+                try:
+                    data_filter = data_filter.query('confidence'.format(search_confidence))
+                except:
+                    pass
+        else:
+            search_lift = ""
+    req = request.args.to_dict()
+
+    if req is not None and 'order[0][column]' in req:
+        order = int(req["order[0][column]"])
+        direction = req["order[0][dir]"] == "asc"
+    if "latt" in req and req["latt"] is not None:
+        leftAttribute = req["latt"]
+    if "ratt" in req and req["ratt"] is not None:
+        rigthAttribute = req["ratt"]
+    if leftAttribute != "None":
+        data_filter = data_filter.query('lhs_attribute == "{}"'.format(leftAttribute))
+    if rigthAttribute != "None":
+        data_filter = data_filter.query('rhs_attribute == "{}"'.format(rigthAttribute))
+
+    to_be_filter = data_filter.columns[order]
+    data_filter.sort_values(by=to_be_filter, axis="index", ascending=direction, inplace=True)
     # Get the number of filtered records
-    total_filtered = len(data_filter)
+    total_filtered = pre_len - len(data_filter)
 
-    # Get the sorting columns and sort the data
-    order_cols = []
-    i = 1
-    while True:
-        col_index = request.args.get(f'order[{i}][column]')
-        if col_index is None:
-            break
-        col_name = request.args.get(f'columns[{col_index}][data]')
-        descending = request.args.get(f'order[{i}][dir]', '') == 'desc'
-        order_cols.append((col_name, descending))
-        i += 1
-
-    if order_cols:
-        data_filter = data_filter.sort_values(order_cols, ignore_index=True)
+    # TODO order attribute
 
     # Get the pagination parameters and slice the data
-    start = request.args.get('start', type=int)
-    length = request.args.get('length', type=int)
-    data_page = data_filter.iloc[start:start+length]
-
+    start_ = request.args.get('start', type=int)
+    length_ = request.args.get('length', type=int)
+    if start_ is not None:
+        start = start_
+    if length_ is not None:
+        length = length_
+    print(data_filter)
+    data_page = data_filter.iloc[start:start + length]
+    print(data_page)
     # Format the response
     response = {
         'data': data_page.to_dict('records'),
         'recordsFiltered': total_filtered,
-        'recordsTotal': len(df_results),
-        'draw': request.args.get('draw', type=int),
+        'recordsTotal': pre_len,
+        'draw': request.args.get('draw', type=int)
     }
 
     return response
+
 
 if __name__ == '__main__':
     app.run()
